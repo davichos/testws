04918022027ef2dc450314cf8310b4a109978673
Davidg <Davidg@192.168.1.180>
2013-09-24 16:49:16 -0500

    INI

--- www/js/WebService.js
+++ www/js/WebService.js
@@ -1,29 +1,32 @@
 
 var canlogin = function() {
-
-    $.soap({
-        url: 'http://adventasoluciones.com.mx/detallistas/public/wsdl/index/soap/getCcmClienteInfo',
-        method: '',
-        appendMethodToURL: false,
-        SOAPAction: 'getCcmClienteInfo',
-        soap12: false,
-        params: 'xml',
-        wss: '<?xml version="1.0" encoding="UTF-8"?><env:Envelope xmlns:env="http://www.w3.org/2003/05/soap-envelope" xmlns:ns1="http://localhost/adccmdev/public/wsdl/index/soap" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:enc="http://www.w3.org/2003/05/soap-encoding"><env:Body><ns1:getCcmClienteInfo env:encodingStyle="http://www.w3.org/2003/05/soap-encoding"><numeroCliente xsi:type="xsd:string">T301</numeroCliente><psw xsi:type="xsd:string">VDBSVk0wNUVZekE9</psw></ns1:getCcmClienteInfo></env:Body></env:Envelope>',
-        namespaceQualifier: 'ns1',
-        namespaceURL: 'http://adventasoluciones.com.mx/detallistas/public/wsdl/index/soap',
-        noPrefix: false,
-        elementName: '',
-        enableLogging: false,
-        request: function(SOAPRequest) {
-            alert('request');
-            alert(SOAPRequest);
-        },
-        success: function(SOAPResponse) {
-            alert('sucess');
-            alert(SOAPResponse);
-        },
-        error: function(SOAPResponse) {
-            alert('Error!');
-        }
-    });
+    document.addEventListener('deviceready', function() {
+        $(document).bind('pageinit', function() {
+            $.soap({
+                url: 'http://adventasoluciones.com.mx/detallistas/public/wsdl/index/soap/getCcmClienteInfo',
+                method: '',
+                appendMethodToURL: false,
+                SOAPAction: 'getCcmClienteInfo',
+                soap12: false,
+                params: 'xml',
+                wss: '<?xml version="1.0" encoding="UTF-8"?><env:Envelope xmlns:env="http://www.w3.org/2003/05/soap-envelope" xmlns:ns1="http://localhost/adccmdev/public/wsdl/index/soap" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:enc="http://www.w3.org/2003/05/soap-encoding"><env:Body><ns1:getCcmClienteInfo env:encodingStyle="http://www.w3.org/2003/05/soap-encoding"><numeroCliente xsi:type="xsd:string">T301</numeroCliente><psw xsi:type="xsd:string">VDBSVk0wNUVZekE9</psw></ns1:getCcmClienteInfo></env:Body></env:Envelope>',
+                namespaceQualifier: 'ns1',
+                namespaceURL: 'http://adventasoluciones.com.mx/detallistas/public/wsdl/index/soap',
+                noPrefix: false,
+                elementName: '',
+                enableLogging: false,
+                request: function(SOAPRequest) {
+                    alert('request');
+                    alert(SOAPRequest);
+                },
+                success: function(SOAPResponse) {
+                    alert('sucess');
+                    alert(SOAPResponse);
+                },
+                error: function(SOAPResponse) {
+                    alert('Error!');
+                }
+            });
+        });
+    }, null);
 };

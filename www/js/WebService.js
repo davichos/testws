74464938b6ad4f2a9ef3e6a12281c20a09786b00
Davidg <Davidg@192.168.1.180>
2013-09-25 10:52:51 -0500

    INI

--- www/js/WebService.js
+++ www/js/WebService.js
@@ -3,10 +3,10 @@ var canlogin = function() {
 //    document.addEventListener('deviceready', function() {
 //        $(document).bind('pageinit', function() {
             var a=$.soap({
-                url: 'http://adventasoluciones.com.mx/detallistas/public/wsdl/index/soap/getCcmClienteInfo',
-                method: '',
+                url: 'http://adventasoluciones.com.mx/detallistas/public/wsdl/index',
+                method: 'http://adventasoluciones.com.mx/detallistas/public/wsdl/index/soap/getCcmClienteInfo',
                 appendMethodToURL: false,
-                SOAPAction: 'http://adventasoluciones.com.mx/detallistas/public/wsdl/index/soap/getCcmClienteInfo',
+                SOAPAction: 'null',
                 soap12: false,
                 wss: {},
                 params:  '<?xml version="1.0" encoding="UTF-8"?><env:Envelope xmlns:env="http://www.w3.org/2003/05/soap-envelope" xmlns:ns1="http://adventasoluciones.com.mx/detallistas/public/wsdl/index/soap" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:enc="http://www.w3.org/2003/05/soap-encoding"><env:Body><ns1:getCcmClienteInfo env:encodingStyle="http://www.w3.org/2003/05/soap-encoding"><numeroCliente xsi:type="xsd:string">T301</numeroCliente><psw xsi:type="xsd:string">VDBSVk0wNUVZekE9</psw></ns1:getCcmClienteInfo></env:Body></env:Envelope>',

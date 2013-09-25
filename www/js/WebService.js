f47daa26806e05d285b47310d941bf4342d681a8
Davidg <Davidg@192.168.1.180>
2013-09-25 10:56:01 -0500

    INI

--- www/js/WebService.js
+++ www/js/WebService.js
@@ -6,7 +6,7 @@ var canlogin = function() {
                 url: 'http://adventasoluciones.com.mx/detallistas/public/wsdl/index',
                 method: 'http://adventasoluciones.com.mx/detallistas/public/wsdl/index/soap/getCcmClienteInfo',
                 appendMethodToURL: false,
-                SOAPAction: 'null',
+                SOAPAction: null,
                 soap12: false,
                 wss: {},
                 params:  '<?xml version="1.0" encoding="UTF-8"?><env:Envelope xmlns:env="http://www.w3.org/2003/05/soap-envelope" xmlns:ns1="http://adventasoluciones.com.mx/detallistas/public/wsdl/index/soap" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:enc="http://www.w3.org/2003/05/soap-encoding"><env:Body><ns1:getCcmClienteInfo env:encodingStyle="http://www.w3.org/2003/05/soap-encoding"><numeroCliente xsi:type="xsd:string">T301</numeroCliente><psw xsi:type="xsd:string">VDBSVk0wNUVZekE9</psw></ns1:getCcmClienteInfo></env:Body></env:Envelope>',
@@ -21,9 +21,10 @@ var canlogin = function() {
                 },
                 success: function(SOAPResponse,a,b) {
                     alert('sucess');
-                    alert(SOAPResponse);
+                    
                     alert(a);
                     alert(b);
+                    alert(SOAPResponse);
                 },
                 error: function(SOAPResponse) {
                     alert('Error!');
